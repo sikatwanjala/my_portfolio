aee13bb97659ca6b62830da73bbec689c573452d
Wanjala-Filex <sikatafilex@gmail.com>
2026-06-26 17:36:40 +0300

    Configure Django settings for Vercel deployment

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static", 
 ]
 
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
